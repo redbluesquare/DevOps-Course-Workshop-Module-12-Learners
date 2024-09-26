0aa2ad0b631d2ca94283cd7bd30b435b8a94debe
Darryl Usher <darryl@redbluesquare.co.uk>
2024-09-26 15:51:02 +0000

    terraform script update

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,18 @@ resource "azurerm_linux_web_app" "main" {
   resource_group_name = data.azurerm_resource_group.main.name
   location            = resource.azurerm_service_plan.main.location
   service_plan_id     = resource.azurerm_service_plan.main.id
-  site_config {}
+  site_config {
+    application_stack {
+      docker_image_name     = "corndeldevopscourse/mod12app:latest"
+      docker_registry_url   = "https://index.docker.io"
+    }
+  }
+
+  app_settings = {
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" : "True"
+    "DEPLOYMENT_METHOD" : "Terraform"
+    "CONNECTION_STRING" : "Server=tcp:darush-non-iac-sqlserver.database.windows.net,1433;Initial Catalog=darush-non-iac-db;Persist Security Info=False;User ID=dbadmin;Password=${var.database_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+  }
 }
 
